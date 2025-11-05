af7275c92a625dce0e607a481f630769209646f5
Devon <ds.pydev@gmail.com>
2025-11-05 14:24:34 +0200

    Finished rewrite of attendants.py and update main.py

---
+++ App/build_excel.py
@@ -0,0 +1,246 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import re
+import attendants as at
+
+
+# ==============================================================================
+# Build Excel
+# ==============================================================================
+
+# Track total hours
+total_hours = {}
+
+def build_excel(roster, week_dates, week_data, week=1): 
+	# FUNCTIONS
+	# Find numbers before dash and after dash
+	def first(weekday):
+		# print(weekday)
+		first = float(re.findall('[0-9]+(?=.*\-)', weekday)[0])
+		return first
+
+	def second(weekday):
+		second = float(re.findall('\-(.*)', weekday)[0])
+		return second
+	
+	# # Get employee data
+	# if week == 1:
+	# 	week_data = week_one_info()
+	# else:
+	# 	week_data = week_two_info()
+	
+	# ##############################################
+	# BUILD OUT EXCEL FILE
+	# ##############################################
+
+	# Start workbook or continue
+	if week == 1:
+		wb = Workbook()
+		wb.active.title = 'Attendents'
+		ws = wb['Attendents']
+	else:
+		wb = load_workbook("Total Time Worked.xlsx")
+		ws = wb['Attendents']
+
+	# Get days of week
+	# if week == 1:
+	# 	thursday = weekone_dates["Thursday"]
+	# 	friday = weekone_dates["Friday"]
+	# 	saturday = weekone_dates["Saturday"]
+	# 	sunday = weekone_dates["Sunday"]
+	# 	monday = weekone_dates["Monday"]
+	# 	tuesday = weekone_dates["Tuesday"]
+	# 	wednesday = weekone_dates["Wednesday"]
+	# else:
+	# 	thursday = weektwo_dates["Thursday"]
+	# 	friday = weektwo_dates["Friday"]
+	# 	saturday = weektwo_dates["Saturday"]
+	# 	sunday = weektwo_dates["Sunday"]
+	# 	monday = weektwo_dates["Monday"]
+	# 	tuesday = weektwo_dates["Tuesday"]
+	# 	wednesday = weektwo_dates["Wednesday"]
+
+	if week == 1:
+		thursday = week_dates["Thursday"]
+		friday = week_dates["Friday"]
+		saturday = week_dates["Saturday"]
+		sunday = week_dates["Sunday"]
+		monday = week_dates["Monday"]
+		tuesday = week_dates["Tuesday"]
+		wednesday = week_dates["Wednesday"]
+	else:
+		thursday = week_dates["Thursday"]
+		friday = week_dates["Friday"]
+		saturday = week_dates["Saturday"]
+		sunday = week_dates["Sunday"]
+		monday = week_dates["Monday"]
+		tuesday = week_dates["Tuesday"]
+		wednesday = week_dates["Wednesday"]
+
+	# Headings
+	if week == 1:
+		hd_row = 1
+		shd_row = 2
+	else:
+		hd_row = 16
+		shd_row = 17
+
+	ws[f'A{hd_row}'] = 'Week 1'
+	ws[f'B{hd_row}'] = thursday
+	ws[f'C{hd_row}'] = friday
+	ws[f'D{hd_row}'] = saturday
+	ws[f'E{hd_row}'] = sunday
+	ws[f'F{hd_row}'] = monday
+	ws[f'G{hd_row}'] = tuesday
+	ws[f'H{hd_row}'] = wednesday
+
+	ws[f'A{shd_row}'] = 'ATTENDENTS'
+	ws[f'B{shd_row}'] = 'THURS'
+	ws[f'C{shd_row}'] = 'FRI'
+	ws[f'D{shd_row}'] = 'SAT'
+	ws[f'E{shd_row}'] = 'SUN'
+	ws[f'F{shd_row}'] = 'MON'
+	ws[f'G{shd_row}'] = 'TUE'
+	ws[f'H{shd_row}'] = 'WED'
+
+	ws[f'J{shd_row}'] = 'Hours'
+	ws[f'K{shd_row}'] = 'Sun Hours'
+
+	ws['M17'] = 'Name'
+	ws['N17'] = 'Total Hours'
+	ws['O17'] = 'Total Sun Hours'
+
+	# Row and column start
+	if week == 1:
+		row = 3
+	else: 
+		row = 18	
+
+	# Extract data to excel
+	i = 0
+
+	for r in week_data:
+		# print(r)
+		name = r[0][0]
+		thur = r[0][2]
+		fri = r[0][3]
+		sat = r[0][4]
+		sun = r[0][5]
+		mon = r[0][6]
+		tue = r[0][7]
+		wed = r[0][8]
+
+		ws[f'A{row + i}'] = name
+
+		if thur == 'AF':
+			th = 0
+			ws[f'B{row + i}'] = f'{thur} | ({int(th)})'
+		elif first(thur) >= 18:
+			th = (24 - first(thur)) + second(thur)
+			ws[f'B{row + i}'] = f'{thur} | ({int(th)})'
+		elif first(thur) - second(thur) < 0:
+			th = (first(thur) - second(thur)) * -1
+			ws[f'B{row + i}'] = f'{thur} | ({int(th)})'
+		else:
+			th = first(thur) - second(thur)
+			ws[f'B{row + i}'] = f'{thur} | ({int(th)})'
+		
+		if fri == 'AF':
+			fr = 0
+			ws[f'C{row + i}'] = f'{fri} | ({int(fr)})'
+		elif first(fri) >= 18:
+			fr = (24 - first(fri)) + second(fri)
+			ws[f'C{row + i}'] = f'{fri} | ({int(fr)})'
+		elif first(fri) - second(fri) < 0:
+			fr = (first(fri) - second(fri)) * -1
+			ws[f'C{row + i}'] = f'{fri} | ({int(fr)})'
+		else:
+			fr = first(fri) - second(fri)
+			ws[f'C{row + i}'] = f'{fri} | ({int(fr)})'
+
+		# Certain employees work some sunday hours that have a different pay structure var_n
+		global sun_wo
+		if sat == 'AF':
+			sa = 0
+			sun_wo = 0
+			ws[f'D{row + i}'] = f'{sat} | ({int(sa)})'
+		elif sat == '18-6' or sat == '18-7':
+			sun_wo = second(sat)
+			sa = (24 - first(sat))
+			ws[f'D{row + i}'] = f'{sat} | ({int(sa)})'
+		else:
+			sun_wo = 0
+			sa = (first(sat) - second(sat)) * -1
+			ws[f'D{row + i}'] = f'{sat} | ({int(sa)})'
+
+		global mon_wo
+		if sun == 'AF':
+			mon_wo = 0
+			su = 0
+			ws[f'E{row + i}'] = f'{sun} | ({int(su) + int(sun_wo)})'
+		elif sun == '18-6' or sun == '18-7':
+			mon_wo = second(sun)
+			su = (24 - first(sun))
+			ws[f'E{row + i}'] = f'{sun} | ({int(su) + int(sun_wo)})'
+		else:
+			mon_wo = 0
+			su = (first(sun) - second(sun)) * -1
+			ws[f'E{row + i}'] = f'{sun} | ({int(su) + int(sun_wo)})'
+
+		if mon == 'AF':
+			mo = 0
+			ws[f'F{row + i}'] = f'{mon} | ({int(mo) + int(mon_wo)})'
+		elif first(mon) >= 18:
+			mo = (24 - first(mon)) + second(mon)
+			ws[f'F{row + i}'] = f'{mon} | ({int(mo) + int(mon_wo)})'
+		else:
+			mo = (first(mon) - second(mon)) * -1
+			ws[f'F{row + i}'] = f'{mon} | ({int(mo) + int(mon_wo)})'
+		
+		if tue == 'AF':
+			tu = 0
+			ws[f'G{row + i}'] = f'{tue} | ({int(tu)})'
+		elif first(tue) >= 18:
+			tu = (24 - first(tue)) + second(tue)
+			ws[f'G{row + i}'] = f'{tue} | ({int(tu)})'
+		else:
+			tu = (first(tue) - second(tue)) * -1
+			ws[f'G{row + i}'] = f'{tue} | ({int(tu)})'
+
+		if wed == 'AF':
+			we = 0
+			ws[f'H{row + i}'] = f'{wed} | ({int(we)})'
+		elif first(wed) >= 18:
+			we = (24 - first(wed)) + second(wed)
+			ws[f'H{row + i}'] = f'{wed} | ({int(we)})'
+		else:
+			we = (first(wed) - second(wed)) * -1
+			ws[f'H{row + i}'] = f'{wed} | ({int(we)})'
+
+		# Add total hours at end
+		hours = th + fr + sa + mon_wo + mo + tu + we
+		ws[f'J{row + i}'] = f'{int(hours)}'
+
+		sun_hours = int(su) + int(sun_wo)
+		ws[f'K{row + i}'] = f'{int(sun_hours)}'
+
+		# Add total hours to dic
+		if week == 1:
+			total_hours[name] = [int(hours), int(sun_hours)]
+		else:
+			total_hours[name][0] += int(hours) 
+			total_hours[name][1] += int(sun_hours)
+
+		if week == 2:
+			ws[f'M{row + i}'] = name
+			ws[f'N{row + i}'] = total_hours[name][0]
+			ws[f'O{row + i}'] = total_hours[name][1]
+
+		i += 1 
+		
+	# Close workbook
+	wb.save("Total Time Worked.xlsx")
+	wb.close()
+
+build_excel()
+build_excel(2)
--- App/main.py
+++ App/main.py
@@ -4,305 +4,89 @@ from PIL import ImageTk,Image
 import os
 from os import path
 import pandas as pd
-import attendants as a
+import attendants as att
 import cashiers as ca
 import bakery as b
 import sqlite3
 
+# ############# NOTES #############################################
+# If nothing selected do nothing
+# Not make so many connection to database
+# #################################################################
+
 root = Tk()
 
-def attendant_time(selected):
-    index = file_sheets_a.index(attendant_selectbox.get())
-    wo = a.attendant_one(index)
-    wt = a.attendant_two(index)
+def clean_database():
+    con = sqlite3.connect('database/time_Sheet.db')
+    c = con.cursor()
 
-    conn = sqlite3.connect('database/time_Sheet.db')
-    c = conn.cursor()
+    c.execute('DROP TABLE IF EXISTS rosterAttWeekOne')
+    c.execute('DROP TABLE IF EXISTS rosterAttWeekTwo')
 
-    c.execute('CREATE TABLE IF NOT EXISTS attendants (name TEXT, standardHours INTEGER, sundayHours INTEGER)')
-    c.execute('CREATE TABLE IF NOT EXISTS attendantsWT (name TEXT, standardHours INTEGER, sundayHours INTEGER)')
+    con.commit()
+    con.close() 
 
-    c.execute('DELETE FROM attendants')
-    c.execute('DELETE FROM attendantsWT')
+def run_calculator():
+	attendant = attendant_selectbox.get()
+	cashier = cashier_selectbox.get()
+	baker = bakery_selectbox.get()
 
-    # Attendants Times 
-    for x in range(len(wo)):
-        query = ("INSERT INTO attendants (name, standardHours, sundayHours) VALUES (?, ?, ?)")
-        c.execute(query, (wo[x][0], wo[x][1], wo[x][2]))    
-
-    for x in range(len(wt)):
-        query = ("INSERT INTO attendantsWT (name, standardHours, sundayHours) VALUES (?, ?, ?)")
-        c.execute(query, (wt[x][0], wt[x][1], wt[x][2]))
-
-    c.execute("""
-    UPDATE attendants
-    SET
-    standardHours = standardHours + (SELECT attendantsWT.standardHours
-                                            FROM attendantsWT
-                                            WHERE attendantsWT.name = attendants.name),
-    sundayHours = sundayHours + (SELECT attendantsWT.sundayHours
-                                            FROM attendantsWT
-                                            WHERE attendantsWT.name = attendants.name)
-    WHERE EXISTS (
-        SELECT * 
-        FROM attendantsWT
-        WHERE attendantsWT.name = attendants.name
-    )
-    """)
-    
-    c.execute("SELECT * FROM attendants")
-    r = c.fetchall()
-    
-    conn.commit()
-    conn.close() 
-    
-    a_tree = ttk.Treeview(root, height=len(r))
-    # Define columns
-    a_tree['columns'] = ('Name', 'Standard Hours', 'Sunday Hours', 'Total')
-    # formulate columns
-    a_tree.column('#0', width=0)
-    a_tree.column('Name', anchor=W, width=80)
-    a_tree.column('Standard Hours', anchor=CENTER, width=90)
-    a_tree.column('Sunday Hours', anchor=CENTER, width=90)
-    a_tree.column('Total', anchor=CENTER, width=80)
-    # Create Headings
-    a_tree.heading('#0', text='')
-    a_tree.heading('Name', text='Name', anchor=W)
-    a_tree.heading('Standard Hours', text='Standard Hours', anchor=W)
-    a_tree.heading('Sunday Hours', text='Sunday Hours', anchor=W)
-    a_tree.heading('Total', text='Total', anchor=W)
-
-    for x in range(len(r)):
-        name = r[x][0]
-        standard = r[x][1] 
-        sunday = r[x][2]
-        total = r[x][1] + r[x][2]
-        a_tree.insert(parent='', index='end', iid=x, text='', values=(name, standard, sunday, total))
-
-    a_tree.grid(row=2, column=0, columnspan=2, padx=(50,0), pady=(10, 0), sticky=W+E)
-
-def cashier_time(selected):
-    index = file_sheets_c.index(cashier_selectbox.get())
-    cwo = ca.cashier_one(index)
-    cwt = ca.cashier_two(index)
-
-    conn = sqlite3.connect('database/time_Sheet.db')
-    c = conn.cursor()
-
-    c.execute('CREATE TABLE IF NOT EXISTS cashiers (name TEXT, standardHours INTEGER, sundayHours INTEGER)')
-    c.execute('CREATE TABLE IF NOT EXISTS cashiersWT (name TEXT, standardHours INTEGER, sundayHours INTEGER)')
-
-    c.execute('DELETE FROM cashiers')
-    c.execute('DELETE FROM cashiersWT')
-
-    # Cashiers Times
-    for x in range(len(cwo)):
-        query = ("INSERT INTO cashiers (name, standardHours, sundayHours) VALUES (?, ?, ?)")
-        c.execute(query, (cwo[x][0], cwo[x][1], cwo[x][2]))   
-
-    for x in range(len(cwt)):
-        query = ("INSERT INTO cashiersWT (name, standardHours, sundayHours) VALUES (?, ?, ?)")
-        c.execute(query, (cwt[x][0], cwt[x][1], cwt[x][2]))
-
-    c.execute("""
-    UPDATE cashiers
-    SET
-    standardHours = standardHours + (SELECT cashiersWT.standardHours
-                                            FROM cashiersWT
-                                            WHERE cashiersWT.name = cashiers.name),
-    sundayHours = sundayHours + (SELECT cashiersWT.sundayHours
-                                            FROM cashiersWT
-                                            WHERE cashiersWT.name = cashiers.name)
-    WHERE EXISTS (
-        SELECT * 
-        FROM cashiersWT
-        WHERE cashiersWT.name = cashiers.name
-    )
-    """)
-    
-    c.execute("SELECT * FROM cashiers")
-    r = c.fetchall()
-    
-    conn.commit()
-    conn.close() 
-    
-    c_tree = ttk.Treeview(root, height=len(r))
-
-    # Define columns
-    c_tree['columns'] = ('Name', 'Standard Hours', 'Sunday Hours', 'Total')
-
-    # formulate columns
-    c_tree.column('#0', width=0)
-    c_tree.column('Name', anchor=W, width=90)
-    c_tree.column('Standard Hours', anchor=CENTER, width=90)
-    c_tree.column('Sunday Hours', anchor=CENTER, width=90)
-    c_tree.column('Total', anchor=CENTER, width=80)
-
-    # Create Headings
-    c_tree.heading('#0', text='')
-    c_tree.heading('Name', text='Name', anchor=W)
-    c_tree.heading('Standard Hours', text='Standard Hours', anchor=W)
-    c_tree.heading('Sunday Hours', text='Sunday Hours', anchor=W)
-    c_tree.heading('Total', text='Total', anchor=W)
-
-    for x in range(len(r)):
-        name = r[x][0]
-        standard = r[x][1] 
-        sunday = r[x][2]
-        total = r[x][1] + r[x][2]
-        c_tree.insert(parent='', index='end', iid=x, text='', values=(name, standard, sunday, total))
-
-    c_tree.grid(row=4, column=0, columnspan=2, padx=(50,0), pady=(10, 0), sticky=W+E)
-
-def bakery_time(selected):
-    index = file_sheets_c.index(bakery_selectbox.get())
-    bwo = b.bakery_one(index)
-    bwt = b.bakery_two(index)
-    
-    conn = sqlite3.connect('database/time_Sheet.db')
-    c = conn.cursor()
-
-    c.execute('CREATE TABLE IF NOT EXISTS bakery (name TEXT, standardHours INTEGER, sundayHours INTEGER)')
-    c.execute('CREATE TABLE IF NOT EXISTS bakeryWT (name TEXT, standardHours INTEGER, sundayHours INTEGER)')
-
-    c.execute('DELETE FROM bakery')
-    c.execute('DELETE FROM bakeryWT')
-
-    # Cashiers Times
-    for x in range(len(bwo)):
-        query = ("INSERT INTO bakery (name, standardHours, sundayHours) VALUES (?, ?, ?)")
-        c.execute(query, (bwo[x][0], bwo[x][1], bwo[x][2]))   
-
-    for x in range(len(bwt)):
-        query = ("INSERT INTO bakeryWT (name, standardHours, sundayHours) VALUES (?, ?, ?)")
-        c.execute(query, (bwt[x][0], bwt[x][1], bwt[x][2]))
-
-    c.execute("""
-    UPDATE bakery
-    SET
-    standardHours = standardHours + (SELECT bakeryWT.standardHours
-                                            FROM bakeryWT
-                                            WHERE bakeryWT.name = bakery.name),
-    sundayHours = sundayHours + (SELECT bakeryWT.sundayHours
-                                            FROM bakeryWT
-                                            WHERE bakeryWT.name = bakery.name)
-    WHERE EXISTS (
-        SELECT * 
-        FROM bakeryWT
-        WHERE bakeryWT.name = bakery.name
-    )
-    """)
-    
-    c.execute("SELECT * FROM bakery")
-    r = c.fetchall()
-    
-    conn.commit()
-    conn.close() 
-    
-    b_tree = ttk.Treeview(root, height=len(r))
-
-    # Define columns
-    b_tree['columns'] = ('Name', 'Standard Hours', 'Sunday Hours', 'Total')
-
-    # formulate columns
-    b_tree.column('#0', width=0)
-    b_tree.column('Name', anchor=W, width=90)
-    b_tree.column('Standard Hours', anchor=CENTER, width=90)
-    b_tree.column('Sunday Hours', anchor=CENTER, width=90)
-    b_tree.column('Total', anchor=CENTER, width=80)
-
-    # Create Headings
-    b_tree.heading('#0', text='')
-    b_tree.heading('Name', text='Name', anchor=W)
-    b_tree.heading('Standard Hours', text='Standard Hours', anchor=W)
-    b_tree.heading('Sunday Hours', text='Sunday Hours', anchor=W)
-    b_tree.heading('Total', text='Total', anchor=W)
-
-    for x in range(len(r)):
-        name = r[x][0]
-        standard = r[x][1] 
-        sunday = r[x][2]
-        total = r[x][1] + r[x][2]
-        b_tree.insert(parent='', index='end', iid=x, text='', values=(name, standard, sunday, total))
-
-    b_tree.grid(row=6, column=0, columnspan=2, padx=(50,0), pady=(10, 0), sticky=W+E)
-
-def refresh():
-    # Gets selection from drop down
-    refresh_attendant = attendant_selectbox.get()
-    refresh_cashier = cashier_selectbox.get()
-    refresh_bakery = bakery_selectbox.get()
-
-    # Execute functions
-    if refresh_attendant == '':
-        pass
-    else:
-        attendant_time(refresh_attendant)
-    if refresh_cashier == '':
-        pass
-    else:
-        cashier_time(refresh_cashier)
-    if refresh_bakery == '':
-        pass
-    else:
-        bakery_time(refresh_bakery)
-
+	# Clean database
+	clean_database()
+	
+	
 # Test if files are in location
 try:
-    file = path.exists('../CASHIERS_ROSTER.xlsx')
-    if file == False:
-        raise FileNotFoundError
+	file = path.exists('../CASHIERS_ROSTER.xlsx')
+	if file == False:
+		raise FileNotFoundError
 except FileNotFoundError:
-    error_label = Label(root, text='File CASHIERS_ROSTER.xls not found')
-    error_label.grid(row=1, column=0, sticky=N+E+S+W, pady=(2, 0), padx=(5, 0))
+	error_label = Label(root, text='File CASHIERS_ROSTER.xls not found')
+	error_label.grid(row=1, column=0, sticky=N+E+S+W, pady=(2, 0), padx=(5, 0))
 else:
-    try:
-        file = path.exists('../Attendant_Carwash_Roster.xlsx')
-        if file == False:
-            raise FileNotFoundError
-    except FileNotFoundError:
-        error_label = Label(root, text='File Attebdant_Carwash_Roster.xls not found')
-        error_label.grid(row=1, column=0, sticky=N+E+S+W, pady=(2, 0), padx=(5, 0))
-    else:
-        file_a = '../Attendant_Carwash_Roster.xlsx'
-        file_sheets_a = pd.ExcelFile(file_a).sheet_names
-        
-        file_c = '../CASHIERS_ROSTER.xlsx'
-        file_sheets_c = pd.ExcelFile(file_c).sheet_names
+	try:
+		file = path.exists('../Attendant_Carwash_Roster.xlsx')
+		if file == False:
+			raise FileNotFoundError
+	except FileNotFoundError:
+		error_label = Label(root, text='File Attebdant_Carwash_Roster.xls not found')
+		error_label.grid(row=1, column=0, sticky=N+E+S+W, pady=(2, 0), padx=(5, 0))
+	else:
+		file_a = '../Attendant_Carwash_Roster.xlsx'
+		file_sheets_a = pd.ExcelFile(file_a).sheet_names
+		
+		file_c = '../CASHIERS_ROSTER.xlsx'
+		file_sheets_c = pd.ExcelFile(file_c).sheet_names
 
-        db_dir = path.exists('database')
-        if db_dir == False:
-            os.mkdir('database')
+		db_dir = path.exists('database')
+		if db_dir == False:
+			os.mkdir('database')
 
+		# LABEL AND COMBOBOX
+		# Attendant time
+		attendant_selectbox_label = Label(root, text='Select Roster Sheet for Attendant Times')
+		attendant_selectbox_label.grid(row=0, column=0, padx=(50,0), pady=(10,0), sticky=W)
 
-        # LABEL AND COMBOBOX
-        # Refresh
-        refresh_button = ttk.Button(root, text="Refresh", command=refresh)
-        refresh_button.grid(row=0, column=0, columnspa=2, padx=(50,0), pady=(10,0), sticky=W+E)
+		attendant_selectbox = ttk.Combobox(root, value=file_sheets_a)
+		attendant_selectbox.grid(row=0, column=1, pady=(10,0), sticky=E)
+		
+		# Cashier Times
+		cashier_selectbox_label = Label(root, text='Select Roster Sheet for Cashier Times')
+		cashier_selectbox_label.grid(row=1, column=0, padx=(50,0), pady=(10,0), sticky=W)
 
-        # Attendant time
-        attendant_selectbox_label = Label(root, text='Select Roster Sheet for Attendant Times')
-        attendant_selectbox_label.grid(row=1, column=0, padx=(50,0), pady=(10,0), sticky=W)
+		cashier_selectbox = ttk.Combobox(root, value=file_sheets_c)
+		cashier_selectbox.grid(row=1, column=1, pady=(10,0), sticky=E)
 
-        attendant_selectbox = ttk.Combobox(root, value=file_sheets_a)
-        attendant_selectbox.bind('<<ComboboxSelected>>', attendant_time)
-        attendant_selectbox.grid(row=1, column=1, pady=(10,0), sticky=E)
-        
-        # Cashier Times
-        cashier_selectbox_label = Label(root, text='Select Roster Sheet for Cashier Times')
-        cashier_selectbox_label.grid(row=3, column=0, padx=(50,0), pady=(10,0), sticky=W)
+		# Bakery Times
+		bakery_selectbox_label = Label(root, text='Select Roster Sheet for Bakery Times')
+		bakery_selectbox_label.grid(row=2, column=0, padx=(50,0), pady=(10,0), sticky=W)
 
-        cashier_selectbox = ttk.Combobox(root, value=file_sheets_c)
-        cashier_selectbox.bind('<<ComboboxSelected>>', cashier_time)
-        cashier_selectbox.grid(row=3, column=1, pady=(10,0), sticky=E)
+		bakery_selectbox = ttk.Combobox(root, value=file_sheets_c)
+		bakery_selectbox.grid(row=2, column=1, pady=(10,0), sticky=E)
 
-        # Bakery Times
-        bakery_selectbox_label = Label(root, text='Select Roster Sheet for Bakery Times')
-        bakery_selectbox_label.grid(row=5, column=0, padx=(50,0), pady=(10,0), sticky=W)
-
-        bakery_selectbox = ttk.Combobox(root, value=file_sheets_c)
-        bakery_selectbox.bind('<<ComboboxSelected>>', bakery_time)
-        bakery_selectbox.grid(row=5, column=1, pady=(10,0), sticky=E)
+		# Run Time Calculator
+		run_button = ttk.Button(root, text="Run Calculator", command=run_calculator)
+		run_button.grid(row=3, column=0, columnspa=2, padx=(50,0), pady=(10,0), sticky=W+E)
 
 # Window layout
 root.title('Calculate Roster Time')
